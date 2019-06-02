319341cf5dceb6721b6e02ab45eb849e3b28507e
Vinayak Hegde <vinayak.hegde@playtech.com>
2019-06-02 20:29:13 +0100

    Initial build subtree commit

---
+++ precache-manifest.d2a02929774a283ba78d53203be41855.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a7bc841c5b063be9f41b1ee794fef55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb1ebe7bdfc4f2812c60",
+    "url": "/static/css/main.65696a65.chunk.css"
+  },
+  {
+    "revision": "10e86d9753757e26a9e1",
+    "url": "/static/js/2.64fc768d.chunk.js"
+  },
+  {
+    "revision": "cb1ebe7bdfc4f2812c60",
+    "url": "/static/js/main.1e3ac59e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "22dea327ee01834ed61a029fbd0d3b08",
+    "url": "/static/media/skiddle-logo.22dea327.png"
+  }
+]);
